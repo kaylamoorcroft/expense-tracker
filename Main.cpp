1326c6aee43ff1adba7d387e7466dc67307a3c01
kaylamoorcroft <kayla.moorcroft@icloud.com>
2025-11-26 16:54:42 -0400

    feat: menu

--- Main.cpp
+++ Main.cpp
@@ -42,26 +42,95 @@ Transaction* parseRecord(char* record) {
     }
 }
 
-// testing
+void displayMenu() {
+    cout << "\n-------- MENU --------" << endl;
+    cout << "1) Load file" << endl;
+    cout << "2) Save file" << endl;
+    cout << "3) Add transaction" << endl;
+    cout << "4) Delete transaction" << endl;
+    cout << "5) Edit transaction" << endl;
+    cout << "6) Get transaction" << endl;
+    cout << "7) Display spreadsheet" << endl;
+    cout << "8) Get stats" << endl;
+    cout << "\nPlease select an operation (enter 0 to quit) > ";
+}
+
 int main() {
-    Income i(2020,5,3,50,"tutoring");
-    Expense e(2020,5,4, 40, "food");
-    string record1 = i.toString();
-    char testRecord1[30];
-    strcpy(testRecord1, record1.c_str()); 
-    Spreadsheet s;
-    s.addEntry(parseRecord(testRecord1));
-    //s.addEntry(&i);
-    s.addEntry(new Expense(30, "rent"));
-    s.addEntry(&e);
-    s.addEntry(new Income(20, "tutoring"));
-    s.addEntry(new Income(2022,5,3,100000, "big moneys"));
-    string r = "2022/01/01,50.00,hewwo";
-    char testRecord[30];
-    strcpy(testRecord, r.c_str()); 
-    s.addEntry(parseRecord(testRecord));
-    cout << "initial: " << endl;
-    s.display();
+    cout << "~~ WELCOME TO EXPENSE TRACKER ~~" << endl;
+    Spreadsheet sheet;
+    int option;
+    string filename;
+    do {
+        displayMenu();
+        cin >> option;
+
+        switch(option) {
+            case 0: // quit
+                cout << "\nGoodbye!" << endl;
+                break;
+            case 1: // load file
+                cout << "\tEnter filename > ";
+                cin >> filename;
+                // code to load filename
+                cout << "\nLoading " << filename << "..." << endl; // replace with function
+                break;
+            case 2: // save file
+                cout << "\tEnter filename > ";
+                cin >> filename;
+                // code to save to file
+                cout << "\nSaving " << filename << "..." << endl; // replace with function
+                break;
+            case 3: // add
+                // call function to get input and add transaction
+                cout << "\nAdding transaction..." << endl; // replace with function
+                break;
+            case 4: // delete
+                sheet.deleteEntry();
+                break;
+            case 5: // edit 
+                sheet.updateEntry();
+                break;
+            case 6: // get
+                sheet.getEntry();
+                break;
+            case 7: // display
+                sheet.display();
+                break;
+            case 8: // stats
+                // call stats summary function
+                cout << "\nSummary..." << endl; // replace with function
+                break;
+            default: // invalid option
+                cout << "\nInvalid selection... please try again" << endl;
+        }
+    } while (option != 0);
+
+
+
+
+
+
+
+    // testing
+
+    // Income i(2020,5,3,50,"tutoring");
+    // Expense e(2020,5,4, 40, "food");
+    // string record1 = i.toString();
+    // char testRecord1[30];
+    // strcpy(testRecord1, record1.c_str()); 
+    // Spreadsheet s;
+    // s.addEntry(parseRecord(testRecord1));
+    // //s.addEntry(&i);
+    // s.addEntry(new Expense(30, "rent"));
+    // s.addEntry(&e);
+    // s.addEntry(new Income(20, "tutoring"));
+    // s.addEntry(new Income(2022,5,3,100000, "big moneys"));
+    // string r = "2022/01/01,50.00,hewwo";
+    // char testRecord[30];
+    // strcpy(testRecord, r.c_str()); 
+    // s.addEntry(parseRecord(testRecord));
+    // cout << "initial: " << endl;
+    // s.display();
     // cout << "\n--from nov 2025: " << endl;
     // for (Transaction* t : s.filterMonth(2025, 11)) {
     //     t->display();
